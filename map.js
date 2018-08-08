e1ade60dd53491a5ee79ebae8c529e60caff5c32
karla2aguilar <karla.5225.aguilar@gmail.com>
2018-08-08 09:16:33 -0400

    Adding map and buttons

--- map.js
+++ map.js
@@ -36,3 +36,45 @@ window.onload = init;
 
 // });
 
+
+function panHome() {
+    view.animate({
+        center: ourLoc,
+        duration: 2000
+    });
+};
+
+function panToLocation() {
+    var countryName = document.getElementById("country-name").value;
+    
+    if(countryName === "") {
+        alert("You didn't enter a country name!");
+        return;
+    };
+
+    var lon = 0.0;
+    var lat = 0.0;
+
+    var query = "https://restcountries.eu/rest/v2/name/"+countryName;
+    query = query.replace(/ /g, "%20");
+    alert(query); 
+
+    var countryRequest = new XMLHttpRequest();
+    countryRequest.open('GET', query, false);
+
+    countryRequest.send();
+
+    var countryInformation = JSON.parse(countryRequest.response(Text));
+
+    var lat = countryInfromation[0].latlng[0];
+    var lon = countryInfromation[0].lonlng[0];
+
+    var location = ol.proj.fromLonLat ([lon, lat]);
+
+    view.animate({
+        center: location,
+        duration: 2000
+    });
+
+}
+
